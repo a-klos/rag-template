f5cb579c9e100c4cff2658d688ad1e3e2ebcc707
Andreas Klos <andreas.klos@stackit.cloud>
2025-07-18 14:52:26 +0200

    feat: comment out uuid utility export in index.ts

--- services/frontend/libs/shared/utils/src/index.ts
+++ services/frontend/libs/shared/utils/src/index.ts
@@ -3,4 +3,4 @@ export * from './lib/file-size-formatter.utils'
 export * from './lib/is-empty.utils'
 export * from './lib/marked.utils'
 export * from './lib/uuid.util'
-
+// export * from './lib/uuid.util'
